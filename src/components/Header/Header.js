cf80e99cc5138cc641b9b3c548ea2893168cd1ba
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2021-09-04 21:17:11 +0530

    Added:Hero Section ,Responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { HiOutlineMail } from "react-icons/hi";
 import { DiCssdeck } from "react-icons/di";
 
 import {
@@ -10,15 +11,23 @@ import {
   Div3,
   NavLink,
   SocialIcons,
+  Span,
 } from "./HeaderStyles";
 
 const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "20px",
+          }}
+        >
           <DiCssdeck size="3rem" />
-          <span>Portofolio</span>
+          <Span>Portofolio</Span>
         </a>
       </Link>
     </Div1>
@@ -39,6 +48,17 @@ const Header = () => (
         </Link>
       </li>
     </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/whodissaket">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/saket-nandanwar-70b838125/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="mailto:saketnandanwar@gmail.com">
+        <HiOutlineMail size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi! I'm <br />
+        Saket Nandanwar
+      </SectionTitle>
+      <SectionText>
+        I'm a <b>Full Stack Developer and a student</b> based in{" "}
+        <b>Mumbai, India</b>.
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
